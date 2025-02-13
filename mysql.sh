7d839bb4a4397ebdbfbf41f955bdd6f42442c10c
akhildommati <akhilprince567@gmail.com>
2025-02-12 22:42:59 -0500

    expense shell

---
+++ mysql.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+echo -e "$2...... $R FAILURE $N"
+exit 1
+else 
+echo  -e "$2...... $G SUCCESS  $N"
+fi
+}
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]
+then
+echo "ERROR ::You need to have root privileges to run this script"
+exit 1 # other than zero
+fi
+}
+
+echo "Script started executing at : $TIMESTAMP" &>>$LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing mysql-server"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling mysql-server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Starting mysql-server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting Root Password"
