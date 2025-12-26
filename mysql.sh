e378dac2ce2faf29db890153d76c95cb15e13b26
akhil <akhilprince567@gmail.com>
2025-12-26 16:44:57 -0500

    expense-shell

---
+++ mysql.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S )
+LOG_FILE_NAME="$LOGS_FOLDER/"$LOG_FILE"-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 ......$R FAILURE $N"
+    exit 1
+    else
+    echo -e "$2 ......$G SUCCESS $N"
+    fi  
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR : you must have sudo access to execute this script."
+        exit 1
+    fi  
+}
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : you must have sudo access to execute this script."
+    exit 1
+fi  
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Mysql Server"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Mysqld Service"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Mysqld Service"
+
+mysql_secure_installation --set-root-pass ExpenseApp@123 &>>$LOG_FILE_NAME
+VALIDATE $? "Securing Mysqld Installation"
+
+mysql -h mysql.akhildommati.fun -u root -p<password> &>>$LOG_FILE_NAME
+VALIDATE $? "Connecting to Mysqld"
+
+show databases; &>>$LOG_FILE_NAME
+VALIDATE $? "Showing Databases"
+
+show tables; &>>$LOG_FILE_NAME
+VALIDATE $? "Showing Tables"
+
+select * from <table_name>; &>>$LOG_FILE_NAME
+VALIDATE $? "Selecting Data from Table"
