b07de299b4f4d9cfd8c6a154b6fc8b6897c05b1f
akhil <akhilprince567@gmail.com>
2025-12-26 19:46:51 -0500

    expense-shell

---
+++ backend.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S )
+LOG_FILE_NAME="$LOGS_FOLDER/"$LOG_FILE"-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 ......$R FAILURE $N"
+    exit 1
+    else
+    echo -e "$2 ......$G SUCCESS $N"
+    fi  
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR : you must have sudo access to execute this script."
+        exit 1
+    fi  
+}
+
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Disabling Nodejs Module"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Nodejs 20 Module"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Nodejs"
+
+useradd expenseapp &>>$LOG_FILE_NAME
+VALIDATE $? "Adding ExpenseApp User"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE $? "Creating /app Directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading Backend Zip File"
+
+cd /app &>>$LOG_FILE_NAME
+VALIDATE $? "Changing Directory to /app"
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Unzipping Backend Files"
+
+cd /app/backend &>>$LOG_FILE_NAME
+VALIDATE $? "Changing Directory to /app/backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing NPM Packages"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE_NAME
+VALIDATE $? "Copying Backend Service File"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Reloading Systemd Daemon"
+
+systemctl start backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend Service"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Backend Service"
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Mysql Client"
+
+mysql -h mysql.akhildommati.fun -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Creating Expense Database and Tables"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "Restarting Backend Service"
