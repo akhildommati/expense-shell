01e85b47cf870dadfc8fea7bf6d99261af3572b7
akhil <akhilprince567@gmail.com>
2025-12-26 22:22:25 -0500

    expense-shell

--- frontend.sh
+++ frontend.sh
@@ -29,6 +29,9 @@ CHECK_ROOT(){
     fi  
 }
 
+mkdir -p $LOGS_FOLDER   &>>$LOG_FILE_NAME
+VALIDATE $? "Creating Logs Folder"
+
 echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
